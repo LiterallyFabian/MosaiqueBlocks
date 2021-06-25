628d2e8c5520b299c4e498a9b106f53a7ef349e3
Fabian <lindgrenfabian@gmail.com>
2021-06-26 00:21:20 +0200

    add method to create block

--- MosaiqueBlocks/Program.cs
+++ MosaiqueBlocks/Program.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
 
 namespace MosaiqueBlocks
 {
     internal class Program
     {
+        static List<TextureItem> TextureItems;
         private static void Main(string[] args)
         {
             Console.Write("Path (assets/minecraft/textures):");
             string path = Console.ReadLine();
             path = path == "" ? "assets/minecraft/textures" : path;
-
             string[] files = Directory.GetFiles(path, "*.png", SearchOption.AllDirectories);
+            TextureItems = GetTextureItems(files);
+
+            foreach(string file in files) CreateMosaiqueBlock(file);
+        }
+
+        private static List<TextureItem> GetTextureItems(string[] files)
+        {
             Console.WriteLine($"Processing {files.Length} images...");
 
             List<TextureItem> textures = new List<TextureItem>();
@@ -24,14 +33,16 @@ namespace MosaiqueBlocks
             }
 
             Console.WriteLine($"{textures.Count} images without transparancy found.");
+            return textures;
         }
 
         /// <summary>
         /// Creates a TextureItem if image does not contain transparent areas
         /// </summary>
-        public static TextureItem ContainsTransparent(string path)
+        private static TextureItem ContainsTransparent(string path)
         {
             Bitmap image = new Bitmap(path);
+            if (image.Height != 16 || image.Height != 16) return null;
             //loop through all pixels in image and check alpha level
             for (int y = 0; y < image.Height; ++y)
             {
@@ -44,6 +55,43 @@ namespace MosaiqueBlocks
                 }
             }
             return new TextureItem(image, path);
+        }
+
+        private static void CreateMosaiqueBlock(string path)
+        {
+            Bitmap sourceImage = new Bitmap(path);
+            Bitmap mosaiqueImage = new Bitmap(sourceImage.Width * 16, sourceImage.Height * 16);
+            using (Graphics g = Graphics.FromImage(mosaiqueImage))
+            {
+                for (int y = 0; y < sourceImage.Height; ++y)
+                {
+                    for (int x = 0; x < sourceImage.Width; ++x)
+                    {
+                        Color pixel = sourceImage.GetPixel(x, y);
+                        if (pixel.A > 255 / 2)
+                        {
+                            g.DrawImage(TextureItems[closestColor(pixel)].Bitmap, new Point(x*16, y*16));
+                        }
+                    }
+                }
+                string newPath = "output/" + path;
+                Directory.CreateDirectory(Path.GetDirectoryName(newPath));
+                mosaiqueImage.Save(newPath, ImageFormat.Png);
+                Console.WriteLine($"Block {path} created.");
+            }
+        }
+
+        private static int closestColor(Color target)
+        {
+            var colorDiffs = TextureItems.Select(n => ColorDiff(n.AverageColor, target)).Min(n => n);
+            return TextureItems.FindIndex(n => ColorDiff(n.AverageColor, target) == colorDiffs);
+        }
+
+        private static int ColorDiff(Color c1, Color c2)
+        {
+            return (int)Math.Sqrt((c1.R - c2.R) * (c1.R - c2.R)
+                                   + (c1.G - c2.G) * (c1.G - c2.G)
+                                   + (c1.B - c2.B) * (c1.B - c2.B));
         }
     }
 }
